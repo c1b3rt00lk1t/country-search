f7a842ec9bb938fdb07a04e7091dd78052998d1f
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-10-10 21:30:14 +0200

    Added a new state for countryDB and useEffect to retrieve the data from the server with axios and set the state with it.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import axios from 'axios';
 import {useState, useEffect} from 'react';
 import Search from "./components/Search";
 
 function App() {
   const [countrySearch, setCountrySearch] = useState('');
+  const [countryDB, setCountryDB] = useState([]);
 
-  const getDataFromServer = () => {};
+  const getDataFromServer = () => {
+    axios
+        .get('https://restcountries.com/v3.1/all')
+        .then(response => setCountryDB(response.data))
+  };
 
   useEffect(getDataFromServer,[]);
 
