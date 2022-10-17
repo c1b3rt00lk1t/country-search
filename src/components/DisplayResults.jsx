6d6250c20ebde49a9cd700ba36c233f936db3515
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-10-17 22:18:24 +0200

    Added a ternary operator to show disclaimer when the list of countries is too long.

--- src/components/DisplayResults.jsx
+++ src/components/DisplayResults.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const DisplayResults = ({countries}) => {
   return (
     <>
-      {countries.map( country => <p>{country.name.official}</p>)}
+      {countries.length >= 10 ? <p>Too many countries</p> : countries.map( country => <p>{country.name.official}</p>)}
     </>
   )
 }
