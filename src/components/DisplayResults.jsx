281b593f878ddbfb02aa9376973363a70d3991e8
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-10-17 23:06:50 +0200

    Received the handleClickShowTag prop and passed it together with the name to the ShowTag component.

--- src/components/DisplayResults.jsx
+++ src/components/DisplayResults.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import ShowTag from './ShowTag'
 import SingleCountry from './SingleCountry'
 
-const DisplayResults = ({countries}) => {
+const DisplayResults = ({countries, handleClickShowTag}) => {
   return (
     <>
       {countries.length >= 10 ? 
           <p>Too many countries</p> : 
           (countries.length === 1 ? 
                 <SingleCountry country={countries[0]}/>: 
-                countries.map( country => <p>{country.name.official} <ShowTag /></p>))
+                countries.map( country => <p>{country.name.official} <ShowTag name={country.name.official} handleClickShowTag={handleClickShowTag}/></p>))
           }
     </>
   )
