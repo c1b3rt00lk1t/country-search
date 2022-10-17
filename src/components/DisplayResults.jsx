8478b2109586b5553473c4ed28e9a111ea060ae1
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-10-17 22:23:51 +0200

    Set up for special display when one specific country is selected.

--- src/components/DisplayResults.jsx
+++ src/components/DisplayResults.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const DisplayResults = ({countries}) => {
   return (
     <>
-      {countries.length >= 10 ? <p>Too many countries</p> : countries.map( country => <p>{country.name.official}</p>)}
+      {countries.length >= 10 ? 
+          <p>Too many countries</p> : 
+          (countries.length === 1 ? 
+                <p>hit</p>: 
+                countries.map( country => <p>{country.name.official}</p>))
+          }
     </>
   )
 }
